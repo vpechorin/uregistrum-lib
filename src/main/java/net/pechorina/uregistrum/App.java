4b17f8477a202e0fe0b79f5ecc580f49715809e2
Victor Pechorin <dev@pechorina.net>
2014-04-28 10:33:44 +1200

    Initial commit

---
+++ src/main/java/net/pechorina/uregistrum/App.java
@@ -0,0 +1,13 @@
+package net.pechorina.uregistrum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
